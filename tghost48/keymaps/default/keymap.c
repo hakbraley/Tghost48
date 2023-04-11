07160ffbd1bb9ed0dbe4d2270c826a9f09580569
hakbraley <65477346+hakbraley@users.noreply.github.com>
2023-04-11 14:27:55 -0400

    fixed keymap errors

--- tghost48/keymaps/default/keymap.c
+++ tghost48/keymaps/default/keymap.c
@@ -18,10 +18,10 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,    KC_K,    KC_L,    KC_LBRC, KC_RBRC,
-        KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-        KC_LCTL,   KC_LOPT, _______, KC_MO(2),KC_MO(1),KC_SPC, KC_MUTE  KC_SPC,  _______, KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT
+        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,    KC_LBRC, KC_RBRC,
+        KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,              KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+        KC_LCTL,   KC_LOPT, _______, MO(2),   MO(1),   KC_SPC,  KC_MUTE,  KC_SPC,  _______, KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT
     ),
     [_FN1] = LAYOUT(
         _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
