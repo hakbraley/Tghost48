2a4acddaf4e7c7de56f4a2a3f5d0808d68036bdc
hakbraley <65477346+hakbraley@users.noreply.github.com>
2023-04-11 11:44:19 -0400

    rgblight_enable ifdef guard

--- tghost48/config.h
+++ tghost48/config.h
@@ -27,31 +27,33 @@
 
 
 /* rgblight definitions */
-#define RGB_DI_PIN D3   // The pin connected to the data pin of the LEDs
-#define RGBLED_NUM 56   // The number of LEDs connected
-#define RGBLIGHT_SLEEP  // RGB lighting will be switched off when the host goes to sleep
-#define RGBLIGHT_LIMIT_VAL 127  // The maximum brightness level (max allowed is 255)
+#ifdef RGBLIGHT_ENABLE
+    #define RGB_DI_PIN D3           // The pin connected to the data pin of the LEDs
+    #define RGBLED_NUM 56           // The number of LEDs connected
+    #define RGBLIGHT_SLEEP          // RGB lighting will be switched off when the host goes to sleep
+    #define RGBLIGHT_LIMIT_VAL 127  // The maximum brightness level (max allowed is 255)
 
-/* enabled rgb animations. comment out the effects you do not want enabled. */
-#define RGBLIGHT_EFFECT_ALTERNATING         // Enable alternating animation mode.
-#define RGBLIGHT_EFFECT_BREATHING	        // Enable breathing animation mode.
-#define RGBLIGHT_EFFECT_CHRISTMAS	        // Enable christmas animation mode.
-#define RGBLIGHT_EFFECT_KNIGHT	            // Enable knight animation mode.
-#define RGBLIGHT_EFFECT_RAINBOW_MOOD	    // Enable rainbow mood animation mode.
-#define RGBLIGHT_EFFECT_RAINBOW_SWIRL	    // Enable rainbow swirl animation mode.
-#define RGBLIGHT_EFFECT_RGB_TEST	        // Enable RGB test animation mode.
-#define RGBLIGHT_EFFECT_SNAKE	            // Enable snake animation mode.
-#define RGBLIGHT_EFFECT_STATIC_GRADIENT	    // Enable static gradient mode.
-#define RGBLIGHT_EFFECT_TWINKLE	            // Enable twinkle animation mode.
+    /* enabled rgb animations. comment out the effects you do not want enabled. */
+    #define RGBLIGHT_EFFECT_ALTERNATING         // Enable alternating animation mode.
+    #define RGBLIGHT_EFFECT_BREATHING	        // Enable breathing animation mode.
+    #define RGBLIGHT_EFFECT_CHRISTMAS	        // Enable christmas animation mode.
+    #define RGBLIGHT_EFFECT_KNIGHT	            // Enable knight animation mode.
+    #define RGBLIGHT_EFFECT_RAINBOW_MOOD	    // Enable rainbow mood animation mode.
+    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL	    // Enable rainbow swirl animation mode.
+    #define RGBLIGHT_EFFECT_RGB_TEST	        // Enable RGB test animation mode.
+    #define RGBLIGHT_EFFECT_SNAKE	            // Enable snake animation mode.
+    #define RGBLIGHT_EFFECT_STATIC_GRADIENT	    // Enable static gradient mode.
+    #define RGBLIGHT_EFFECT_TWINKLE	            // Enable twinkle animation mode.
 
-/* change the order of the LED indexes to match the keymap layout */
-#define RGBLIGHT_LED_MAP {                                  \
-    /* per-key LEDs */                                      \
-    05, 04, 03, 02, 01, 00,         47, 46, 45, 44, 43, 42, \
-    06, 07, 08, 09, 10, 11,         36, 37, 38, 39, 40, 41, \
-    17, 16, 15, 14, 13, 12,         35, 34, 33, 32, 31, 30, \
-    18, 19, 20, 21, 22, 23,         24, 25, 26, 27, 28, 29, \
-    /* underglow LEDs */                                    \
-            54,         55,         48,         49,         \
-            53,         52,         51,         50          \
-}
+    /* change the order of the LED indexes to match the keymap layout */
+    #define RGBLIGHT_LED_MAP {                                  \
+        /* per-key LEDs */                                      \
+        05, 04, 03, 02, 01, 00,         47, 46, 45, 44, 43, 42, \
+        06, 07, 08, 09, 10, 11,         36, 37, 38, 39, 40, 41, \
+        17, 16, 15, 14, 13, 12,         35, 34, 33, 32, 31, 30, \
+        18, 19, 20, 21, 22, 23,         24, 25, 26, 27, 28, 29, \
+        /* underglow LEDs */                                    \
+                54,         55,         48,         49,         \
+                53,         52,         51,         50          \
+    }
+#endif // RGBLIGHT_ENABLE
